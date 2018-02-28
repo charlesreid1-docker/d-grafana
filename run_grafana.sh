55413dc54635db8c25b87309893e6eccdcb1b609
Charles Reid <charlesreid1@gmail.com>
2018-02-28 01:26:13 -0800

    Set grafana server and bind ip

--- run_grafana.sh
+++ run_grafana.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 docker run \
+    --rm \
     -i \
-    -p 3000:3000 \
+    -p 10.6.0.2:3000:3000 \
     -v ${PWD}/grafana-data:/var/lib/grafana \
-    -e "GF_SERVER_ROOT_URL=http://grafana.server.name"  \
+    -e "GF_SERVER_ROOT_URL=http://10.6.0.2"  \
     -e "GF_INSTALL_PLUGINS=grafana-clock-panel,grafana-piechart-panel,grafana-simple-json-datasource 1.2.3" \
     grafana/grafana:latest
 
